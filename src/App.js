0312c998726ca8ab44c3003304e5a8a2980df11d
Pedro Gustavo <eu@pedrogustavo.com>
2020-11-06 23:26:36 -0200

    :globe_with_meridians: Adiciona switch para troca de linguagem

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { useTranslation } from 'react-i18next'
@@ -7,13 +7,30 @@ import { increaseCounter, decreaseCounter } from './redux/Counter/counter.action
 import './App.css'
 
 function App (props) {
-  const { t } = useTranslation()
+  const [language, setLanguage] = useState('pt-BR')
+
+  const { t, i18n } = useTranslation()
+  const changeLanguage = lng => {
+    i18n.changeLanguage(lng)
+  }
+
+  const handleChange = event => {
+    setLanguage(event.target.value)
+    changeLanguage(event.target.value)
+  }
+
   return (
     <div className='App'>
       <p>{t('welcome')}</p>
       <div>Count: {props.count}</div>
+
       <button onClick={() => props.increaseCounter()}>Increase Count</button>
       <button onClick={() => props.decreaseCounter()}>Decrease Count</button>
+
+      <select value={language} onChange={handleChange}>
+        <option value='pt-BR'>pt-BR</option>
+        <option value='en-US'>en-US</option>
+      </select>
     </div>
   )
 }
